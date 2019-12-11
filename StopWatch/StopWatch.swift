0ea71b97e183701062cd97829dc443f960201bee
rhkwj <rhkwj@naver.com>
2019-12-11 17:28:00 +0800

    stopWatch lesson 1~3 https://swifteducation.github.io/teaching_app_development_with_swift/stopwatch.html

---
+++ StopWatch/StopWatch.swift
@@ -0,0 +1,40 @@
+//
+//  StopWatch.swift
+//  StopWatch
+//
+//  Created by Kim Yeon Jeong on 2019/12/11.
+//  Copyright © 2019 Kim Yeon Jeong. All rights reserved.
+//
+
+import Foundation
+
+class Stopwatch {
+    
+    private var startTime: Date?
+
+    var elapsedTime: TimeInterval {
+        if let startTime = self.startTime {
+            return -startTime.timeIntervalSinceNow
+        } else {
+            return 0
+        }
+    }
+    
+    var elapsedTimeAsString: String {
+        return String(format: "%02d:%02d.%d",
+            Int(elapsedTime / 60), Int(elapsedTime.truncatingRemainder(dividingBy: 60)), Int((elapsedTime * 10).truncatingRemainder(dividingBy: 10)))
+    }
+    
+    var isRunning: Bool {
+        return startTime != nil
+    }
+    
+    func start() {
+        startTime = Date()
+    }
+    
+    func stop() {
+        startTime = nil
+    }
+    
+}
--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -4,17 +4,63 @@
 //
 //  Created by Kim Yeon Jeong on 2019/12/11.
 //  Copyright © 2019 Kim Yeon Jeong. All rights reserved.
-//
+// https://swifteducation.github.io/teaching_app_development_with_swift/stopwatch.html
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let rowData = dataArray[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier:
+            "BasicCell", for: indexPath)
+        cell.textLabel?.text = rowData
+        return cell
+    }
+    
 
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    let stopwatch = Stopwatch()
+    
+    
+    @IBAction func lapButtonPressed(_ sender: UIButton) {
+        print("Starting stopwatch")
+        stopwatch.start()
+    }
+    
+    
+    @IBAction func StartButtonPressed(_ sender: UIButton) {
+        print(stopwatch.elapsedTime)
+        stopwatch.stop()
+        
+        //button pressed start to timer
+        
+       
+        //When Srart button pressed change the word to Stop
+
+    }
+    
+ 
+    
+    
+    @IBOutlet weak var myTableView: UITableView!
+    let dataArray = ["number"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        myTableView.delegate = self
+        myTableView.dataSource = self
+        
     }
 
+    
 
 }
 
