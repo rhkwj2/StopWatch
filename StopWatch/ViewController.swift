58e2dfcf7de57bb4749e6da1b45b36821ef11f82
rhkwj <rhkwj@naver.com>
2019-12-12 10:02:24 +0800

    finish stopwatch lesson 5

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -4,7 +4,12 @@
 //
 //  Created by Kim Yeon Jeong on 2019/12/11.
 //  Copyright © 2019 Kim Yeon Jeong. All rights reserved.
-// https://swifteducation.github.io/teaching_app_development_with_swift/stopwatch.html
+
+// 作業 8 - 實作 iOS 時鐘 App 的碼錶和倒數計時器
+//https://medium.com/%E5%BD%BC%E5%BE%97%E6%BD%98%E7%9A%84%E8%A9%A6%E7%85%89-%E5%8B%87%E8%80%85%E7%9A%84-100-%E9%81%93-swift-ios-app-%E8%AC%8E%E9%A1%8C/61-%E5%AF%A6%E4%BD%9C-ios-%E6%99%82%E9%90%98-app-%E7%9A%84%E7%A2%BC%E9%8C%B6%E5%92%8C%E8%A8%88%E6%99%82%E5%99%A8-9a6b9b4288d1
+
+//https://swifteducation.github.io/teaching_app_development_with_swift/stopwatch.html
+// https://stackoverflow.com/questions/24755558/measure-elapsed-time-in-swift
 
 import UIKit
 
@@ -30,9 +35,18 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     @IBAction func lapButtonPressed(_ sender: UIButton) {
         print("Starting stopwatch")
+        Timer.scheduledTimer(timeInterval: 0.1, target: self, selector:#selector(updateElsapsedTimeLabel(timer:)),userInfo: nil, repeats: true)
         stopwatch.start()
+            
     }
-    
+    @objc func updateElsapsedTimeLabel(timer:Timer) {
+        print("updating...")
+        if stopwatch.isRunning {
+        timerLabel.text = "\(stopwatch.elapsedTime)"
+        } else{
+            timer.invalidate()
+        }
+    }
     
     @IBAction func StartButtonPressed(_ sender: UIButton) {
         print(stopwatch.elapsedTime)
